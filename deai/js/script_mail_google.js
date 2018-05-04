ead1deae6babdf5ac8b7c5e0a4c2c67c39ebfc80
DESKTOP-QM0I77H\lasts <b@gmail.com>
2018-05-05 00:20:52 +0900

    出会いサイト作成

---
+++ deai/js/script_mail_google.js
@@ -0,0 +1,321 @@
+$(function() {
+	/*================================================================
+	情報
+	================================================================*/
+
+	var allList = [
+
+	];
+
+	/*================================================================
+	スクリプトはじまり
+	================================================================*/
+	function init() {
+
+		//イベント登録
+		$(".filter_life select").on("change", onFilterChange);
+		$(".filter_category select").on("change", onFilterChange);
+		$(".filter_nayami select").on("change", onFilterChange);
+		$(".filter_tag input").on("change", onFilterChange);
+		$(".filter_keyword button").on("click", onFilterChange);
+
+
+		//最初は全て出力
+		refleshHtml(allList);
+	}
+
+	/*================================================================
+	HTML出力
+	================================================================*/
+	function refleshHtml(list) {
+
+		var outputHtml = '';
+
+		//出力する内容をoutputHtmlに格納
+		if (list.length > 0) {
+			_.each(list, function(line, i) {
+				outputHtml += '<div class="product">';
+				outputHtml += '	<div class="product-left"><div class="photo"><a target="_blank" href="' + line.link + '"><img src="images/' + line.id + '.jpg" alt="' + line.name + '" /></a></div>';
+				outputHtml += '		<div class="tag-list-sp">';
+				outputHtml += '			<ul class="tag">';
+				_.each(line.tag, function(tag, i){
+				outputHtml += '				<li><span>' + tag + '</span></li>';
+				});
+				outputHtml += '			</ul>';
+				outputHtml += '		</div>';
+				outputHtml += '	</div>';
+				outputHtml += '	<div class="info">';
+				outputHtml += '		<h3 class="productTitle">' + line.title + '</h3>';
+				outputHtml += '		<p class="product-star">評価　<img class="star-img" src="images/star-' + line.star + '.png"></p>';
+				outputHtml += '		<div class="tag-list">';
+				outputHtml += '			<ul class="tag">';
+				_.each(line.tag, function(tag, i){
+				outputHtml += '				<li><span>' + tag + '</span></li>';
+				});
+				outputHtml += '			</ul>';
+				outputHtml += '		</div>';
+				outputHtml += '		<div class="detail-list">';
+				outputHtml += '			<table>';
+				outputHtml += '				<tr><td class="amount">サポート体制</td><td>:' + line.life + '時間</td></tr>';
+				outputHtml += '				<tr><td class="warranty">顔出し</td><td>:' + line.warranty + '</td></tr>';
+				outputHtml += '				<tr><td class="list_price">月額最高</td><td>:' + line.list_price + '</td></tr>';
+				outputHtml += '				<tr><td class="price">時給</td><td class="price">:' + line.price + '</td></tr>';
+				outputHtml += '			</table>';
+				outputHtml += '		</div>';
+				outputHtml += '	</div><div class="clear"></div>';
+				outputHtml += '<div class="description-box"><p class="description">' + line.description + '</p></div>';
+				outputHtml += '<div class="conversion"><a class="myButton hvr-grow-shadow" target="_blank" href="' + line.link + '">詳しくはこちら</a></div>';
+				outputHtml += '<div class="clear"></div></div>';
+				outputHtml += '<!--/.product--></div>';
+			});
+		} else 
+
+		//HTML出力（フェードインアニメーションつき）
+		$('.productArea').html(outputHtml);
+		$('.productArea .product').css({opacity: 0}).each(function(i){$(this).delay(100 * i).animate({opacity:1}, 300);
+		});
+
+	}
+
+	/*================================================================
+	絞り込み条件を変更した時
+	================================================================*/
+	function onFilterChange(e) {
+
+		var filterFncs = [];
+		var result = [];
+
+		//セレクトボックスの値を引数に指定した関数filterByLifeをfilterFuncs配列に格納
+		filterFncs.push(
+			function(list) {
+				return filterByLife(list, $('.filter_life select').val());
+			}
+		);
+
+		//セレクトボックスの値を引数に指定した関数filterByCategoryをfilterFuncs配列に格納
+		filterFncs.push(
+			function(list) {
+				return filterByCategory(list, $('.filter_category select').val());
+			}
+		);
+
+		//セレクトボックスの値を引数に指定した関数filterByNayamiをfilterFuncs配列に格納
+		filterFncs.push(
+			function(list) {
+				return filterByNayami(list, $('.filter_nayami select').val());
+			}
+		);
+
+		//チェックボックスの値を引数に指定した関数filterByTagをfilterFuncs配列に格納
+		filterFncs.push(
+			function(list) {
+				return filterByTag(list, $('.filter_tag input:checked'));
+			}
+		);
+
+		//キーワードの値を引数に指定した関数filterByKeywordをfilterFuncs配列に格納
+		filterFncs.push(
+			function(list) {
+				return filterByKeyword(list, _.escape($('.filter_keyword input').val()));
+			}
+		);
+
+		//FilterFuncs配列内の関数をバケツリレーみたいに1つずつ実行して結果をresult配列に格納
+		result = _.reduce(filterFncs, function(list, fnc) {
+			return fnc(list);
+		}, allList);
+
+		//絞り込んだ結果を出力
+		refleshHtml(result);
+	}
+
+	/*================================================================
+	絞り込み[1] セレクトボックス絞り込み関数
+	================================================================*/
+	function filterByLife(list, value) {
+
+		//絞り込み指定がない場合はリターン
+		if (value == "") {
+			return list;
+		}
+
+		//選択したセレクトボックスとlifeがマッチするかでフィルタリング
+		return _.filter(list, function(item) {
+			switch (value) {
+				case '1':
+					return item.life <= 100;
+				case '2':
+					return 100 < item.life && item.life <= 150;
+				case '3':
+					return 150 < item.life && item.life <= 200;
+				case '4':
+					return 200 < item.life;
+			}
+		});
+	}
+
+	function filterByCategory(list, value) {
+
+		//絞り込み指定がない場合はリターン
+		if (value == "") {
+			return list;
+		}
+
+		//選択したセレクトボックスとcategoryがマッチするかでフィルタリング
+		return _.filter(list, function(item) {
+			switch (value) {
+				case '1':
+					return item.category == "メールレディ";
+				case '2':
+					return item.category == "チャットレディ";
+			}
+		});
+	}
+
+	function filterByNayami(list, value) {
+
+		//絞り込み指定がない場合はリターン
+		if (value == "") {
+			return list;
+		}
+
+		//選択したセレクトボックスとnayamiがマッチするかでフィルタリング
+		return _.filter(list, function(item) {
+			switch (value) {
+				case '1':
+					return item.nayami == "顔出しなし";
+				case '2':
+					return item.nayami == "通勤";
+				case '3':
+					return item.nayami == "高時給";
+				case '4':
+					return item.nayami == "振込早い";
+			}
+		});
+	}
+
+	/*================================================================
+	絞り込み[2] チェックボックス絞り込み関数
+	================================================================*/
+	function filterByTag(list, value) {
+
+		//絞り込み指定がない場合はリターン
+		if (value.length == 0) {
+			return list;
+		}
+
+		//選択した属性（チェックボックス）とtagがマッチするかでフィルタリング
+		return _.filter(list, function(item) {
+
+			var isMatch = false;
+
+			//配列同士の比較
+			_.each(value, function(chkItem, i) {
+
+				_.each(item.tag, function(tagItem, i) {
+					if (tagItem === $(chkItem).val()) {
+						isMatch = true;
+					}
+				});
+
+			});
+
+			return isMatch;
+		});
+	}
+
+	/*================================================================
+	絞り込み[3] テキストボックス絞り込み関数
+	================================================================*/
+	function filterByKeyword(list, value) {
+
+		//絞り込み指定がない場合はリターン
+		if (value == "") {
+			return list;
+		}
+
+		//検索キーワードを配列に格納（スペースがある場合は複数格納）
+		var freeAry = [];　
+		var val = value.replace(/　/g, " ");
+		searchAry = val.split(" ");
+
+		//入力したキーワードがtitleもしくはdescriptionにマッチするかでフィルタリング
+		return _.filter(list, function(item) {
+
+			var isMatch = false;
+
+			_.each(searchAry, function(data, i) {
+				if (item.title.indexOf(data) != -1 || item.category.indexOf(data) != -1 || item.nayami.indexOf(data) != -1 || item.description.indexOf(data) != -1) {
+					isMatch = true;
+				}
+			});
+
+			return isMatch;
+
+		});
+
+	}
+
+	/*================================================================
+	スクリプトはじめ
+	================================================================*/
+	init();
+
+});
+
+
+/*================================================================
+クリックでスクロール
+================================================================*/
+
+$(function() {
+  $('div[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 400; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+
+    //ページ内ジャンプ
+    // #で始まるアンカーをクリックした場合に処理
+   $('button[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 400; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+
+   	 //トップへ戻る
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+});
