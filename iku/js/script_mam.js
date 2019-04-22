9ecc93dcae762fecfcd8f137c24a3ea5b1374baf
taroryo1990 <taroryo1990@gmail.com>
2019-04-22 15:08:00 +0900

    サイト更新

--- iku/js/script_mam.js
+++ iku/js/script_mam.js
@@ -47,13 +47,28 @@ $(function() {
 			description: "・30歳以上限定サイト<br>・女性の年齢が高いため、ママ・パトロン探しに最適<br>・Tポイントがたまる制度あり",
 			link: "https://px.a8.net/svt/ejp?a8mat=2ZIWSG+70FU1M+1T5K+1CGWCX"
 		},
+
+		{
+			id: "ikukuru2",
+			name: "イククル",
+			life: "安全",
+			price: "健全にママ活する方",
+			warranty: "大手運営",
+			title: "<a target=\"_blank\" href=\"https://track.bannerbridge.net/click.php?APID=19628&affID=92390&siteID=181045\">4位:イククル</a>",
+			star: "4",
+			category: "育毛剤",
+			nayami: "頭頂部",
+			tag: ["稼げる","人気"],
+			description: "・「年収」で検索できるプロフ検索が便利<br>・女性は登録無料で遊べる<br>・ママ活専用サイトではない、お金持ちの女性が多いのでオススメ",
+			link: "https://track.bannerbridge.net/click.php?APID=19628&affID=92390&siteID=181045"
+		},
 		{
 			id: "merupara2",
 			name: "メルパラ",
 			life: "安全",
 			price: "ママ活する方",
 			warranty: "大手運営",
-			title: "<a target=\"_blank\" href=\"https://track.bannerbridge.net/click.php?APID=159209&affID=92390&siteID=181045\">4位:メルパラ</a>",
+			title: "<a target=\"_blank\" href=\"https://track.bannerbridge.net/click.php?APID=159209&affID=92390&siteID=181045\">5位:メルパラ</a>",
 			star: "4",
 			category: "育毛剤",
 			nayami: "頭頂部",
@@ -62,20 +77,6 @@ $(function() {
 			link: "https://track.bannerbridge.net/click.php?APID=159209&affID=92390&siteID=181045"
 		},
 
-		{
-			id: "ikukuru2",
-			name: "イククル",
-			life: "安全",
-			price: "健全にママ活する方",
-			warranty: "大手運営",
-			title: "<a target=\"_blank\" href=\"https://track.bannerbridge.net/click.php?APID=19628&affID=92390&siteID=181045\">5位:イククル</a>",
-			star: "4",
-			category: "育毛剤",
-			nayami: "頭頂部",
-			tag: ["稼げる","人気"],
-			description: "・「年収」で検索できるプロフ検索が便利<br>・女性は登録無料で遊べる<br>・ママ活専用サイトではない、お金持ちの女性が多いのでオススメ",
-			link: "https://track.bannerbridge.net/click.php?APID=19628&affID=92390&siteID=181045"
-		},
 
 		{
 			id: "lovesearch2",
